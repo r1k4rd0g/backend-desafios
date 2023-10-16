33c3d4e7f8259ae64a3239f9ad5607aa8c12def0
Ricardo Graña <r1k4rd0_0ocho@hotmail.com>
2023-10-16 12:19:50 -0300

    finalizada la función uptdate

--- desafio2.js
+++ desafio2.js
@@ -16,7 +16,7 @@ addProduct debe:
             const products = JSON.parse(productsJSON);
             const existingProduct = products.find((product) => product.code === code);
             if (existingProduct){
-                return console.log(`Ya existe el producto con ${code} ingresado`);
+                throw new Error(`Ya existe el producto con ${code} ingresado`);
             }
             const newProduct={
                 title,
@@ -28,37 +28,47 @@ addProduct debe:
                 id: this.#generateId() + 1,
             };
             products.push(newProduct);
-            await fs.promises.writeFile(this.path, JSON.stringify(products))
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
             return newProduct;
         }catch (error) {
-            return console.log(`Error al agregar el producto: ${error.message}`)
+            throw new Error(`Error al agregar el producto: ${error.message}`)
         }
     }
-    #generateId(){
-        const maxId = data.reduce((prev,current)=>(prev.y>current.y)?prev:current,1)
-        
-        return maxId +1;
+    #generateId(products){
+        const maxId = products.reduce((prev,current)=>(prev.id >current.id ?prev:current), {id:0})
+        return maxId.id +1;
     }
 /*
-getProducts debe devolver todos los elementos que se encuentren dentro del arreglo del archivo json.
+getProducts debe:
+    1-devolver todos los elementos que se encuentren dentro del arreglo del archivo json.
 */
-    async getProductById(idSearch){
-        if(fs.existsSync(this.path)){
-            const productsJSON = await fs.promises.readFile(this.path, 'utf-8');
+    async getProduct(){
+        try {
+            const productsJSON= await fs.promises.readFile(this.path, 'utf-8')
             return JSON.parse(productsJSON);
-        } else {
-            throw new Error(`Error al obtener el archivo: ${this.path} ${e.message}`);
+        } catch (error) {
+            throw new Error (`Error al leer el archivo: ${e.message}`);
         }
     }
-}
-
 /*
 getProductById debe:
     1-recibir por parametro el ID
     2-leer todo el archivo para buscar el ID
     3-devolver en formato objeto. 
 */
-    getProductById();
+    async getProductById(idSearch){
+        try {
+            const productsJSON = await fs.promises.readFile(this.path, 'utf-8');
+            const products = JSON.parse(productsJSON);
+            const product = products.find((product) => product.id === idSearch);
+            if (!product){
+                throw new Error(`No existe producto con ${idSearch} ingresado.`)
+            };
+            return product;
+        } catch (error) {
+            throw new Error (`Error al buscar el producto con ${idSearch}`);
+        };
+    };
 /*
 updateProduct debe:
     1-recibir el ID del producto para actualizar
@@ -66,11 +76,43 @@ updateProduct debe:
     3-actualizar el producto
     4-no borrar el ID
 */
-    updateProduct();
+    async updateProduct(idSearch, updateValues){
+        try {
+            const productsJSON = await fs.promises.readFile(this.path, 'utf-8');
+            const products = JSON.parse(productsJSON);
+            const productFind = products.findIndex((product) =>product.id === idSearch);
+            if (productFind === -1){
+                throw new Error(`El Producto con ${idSearch} no existe.`);
+            }
+            const updateProduct ={
+                ...products[productFind],
+                ...updateValues
+            };
+            products[productFind] = updateProduct;
+            const updateProductJSON = JSON.stringify(products, null, 2);
+            await fs.promises.writeFile(this.path, updateProductJSON);
+            return updateProduct;
+        } catch (error) {
+            throw new Error (`Error al actualizar con el producto con Id ${idSearch}`);
+        }
+    };
 /*
 deleteProduct debe:
     1-recibir el ID del producto
     2-borrar el producto del archivo
     3-crear una array con productos borrados? (opcional)
 */
-    deleteProduct();
+    async deleteProduct(idSearch){
+        try {
+            const productsJSON = await fs.promises.readFile(this.path, 'utf-8');
+            const products = JSON.parse(productsJSON);
+            const productFind = products.findIndex((product) =>product.id === idSearch);
+            if (productFind === -1){
+                throw new Error(`El Producto con Id ${idSearch} no existe.`);
+            }
+        
+        } catch (error) {
+                throw new Error (`Error al eliminar el producto con Id ${idSearch}`);
+        }
+    }
+};
